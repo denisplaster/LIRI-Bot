ae63fee80929f96abd10bbe2f3684d9f6892e7a5
DENIS PLASTER <denjeo@gmail.com>
2018-10-16 12:19:14 -0500

    added photo

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var request = require("request");
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
-// var moment = require("moment");
+var moment = require("moment");
 
 
 
@@ -59,7 +59,7 @@ function getMovie() {
                 "Actors: " + data.Actors
             ].join("\n\n");
 
-            fs.appendFile("log.txt", movieData + "\n---------------------------\n", function (err) {
+            fs.appendFile("random.txt", movieData + "\n---------------------------\n", function (err) {
                 if (err) throw err;
                 console.log("---------------------------\n" +
                     movieData +
@@ -70,43 +70,43 @@ function getMovie() {
 }
 
 
-//BandsInTown =============================================
+//BandsInTown 
 
-// function concertThis() {
+function concertThis() {
 
-//     //Grab user input
-//     var artist = input;
+    //Grab user input
+    var artist = input;
 
-//     //If user doesn't enter artist, return error
-//     if (!artist) {
-//         console.log(red("ERROR: You did not provide an artist!"));
-//         return;
-//     } else {
-//         artist = artist.trim();
-//     }
+    //If user doesn't enter artist, return error
+    if (!artist) {
+        console.log(red("ERROR: You did not provide an artist!"));
+        return;
+    } else {
+        artist = artist.trim();
+    }
 
-    //Search bandsintown for artist
-//     var queryUrl =
-//         "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown;
-//     request(queryUrl, function (error, response, body) {
-//         if (!error && response.statusCode === 200) {
-//             var data = JSON.parse(body);
-//             var formatTime = moment(data[0].datetime.slice(0, 10), "YYYY-MM-DD").format("dddd MMMM Do, YYYY");
-//             var artistData = [
-//                 "Venue Name: " + data[0].venue.name,
-//                 "Venue Location: " + data[0].venue.city + ", " + data[0].venue.country,
-//                 "Date: " + formatTime
-//             ].join("\n\n");
+    // Search bandsintown for artist
+    var queryUrl =
+        "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown;
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            var formatTime = moment(data[0].datetime.slice(0, 10), "YYYY-MM-DD").format("dddd MMMM Do, YYYY");
+            var artistData = [
+                "Venue Name: " + data[0].venue.name,
+                "Venue Location: " + data[0].venue.city + ", " + data[0].venue.country,
+                "Date: " + formatTime
+            ].join("\n\n");
 
-//             fs.appendFile("log.txt", artistData + "\n---------------------------\n", function (err) {
-//                 if (err) throw err;
-//                 console.log(bold.blue("NEXT SHOW FOR: " + artist.toUpperCase()) +
-//                     "\n\n" + artistData +
-//                     "\n---------------------------");
-//             });
-//         }
-//     });
-// }
+            fs.appendFile("random.txt", artistData + "\n---------------------------\n", function (err) {
+                if (err) throw err;
+                console.log(bold.blue("NEXT SHOW FOR: " + artist.toUpperCase()) +
+                    "\n\n" + artistData +
+                    "\n---------------------------");
+            });
+        }
+    });
+}
 
 //Spotify This Song ========================================
 
@@ -144,7 +144,7 @@ function getSong() {
             "Preview link: " + preview
         ].join("\n\n");
 
-        fs.appendFile("log.txt", songData + "\n---------------------------\n", function (err) {
+        fs.appendFile("random.txt", songData + "\n---------------------------\n", function (err) {
             if (err) throw err;
             console.log("---------------------------\n" +
                 songData +
@@ -159,7 +159,7 @@ function doWhatItSays() {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-            return console.log(red("ERROR: " + error));
+            return console.log(red("ERROR" + error));
         }
         else {
             var array = data.split(',');
